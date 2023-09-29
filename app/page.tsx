5e6d8ab9d2f7b5c51bd2755628f11fd4dffdd12f
Fernando Morales <fernando.zmorales@gmail.com>
2023-09-29 18:11:37 -0500

    Added product fixed card shadows

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Container, Col } from "@citrica/objects";
-import Card from "@/components/citrica-ui/card";
 import Navbar from "@/components/citrica-ui/navbar";
+import ProductsList from "@/components/product-list";
 
 export default function Home() {
 	return (
@@ -21,84 +21,7 @@ export default function Home() {
 				</Container>
 			</section>
 			<section id="our-products" className="our-products">
-				<Container>
-					<Col cols={{ lg: 12, md: 6, sm: 4 }}>
-						<h2 className="headline text-gold-brand mb-12">Nuestros Productos</h2>
-					</Col>
-					<Col cols={{ lg: 6, md: 6, sm: 4 }} className="">
-						<div className="py-6">
-							<p className="text-[rgba(29,29,27,1)]">Lorem ipsum dolor sit amet consectetur. Hendrerit eget tempus arcu ultricies tellus dolor cras lorem non. Lectus aenean vitae integer vitae maecenas fames enim malesuada bibendum. Fringilla nulla tortor nunc sed aliquam turpis nisl felis. Hendrerit congue egestas lobortis leo eget vulputate. Diam a eu nec tempus. Magna lobortis est ante at maecenas quam in amet nisl. Consequat nec vulputate enim urna iaculis quisque malesuada nulla eu. Augue aliquet accumsan ac nunc venenatis pretium tortor pharetra.</p>
-						</div>
-					</Col>
-					<Col cols={{ lg: 6, md: 6, sm: 4 }}>
-						<div className="py-6 ">
-							<p className="text-[rgba(29,29,27,1)]">Lorem ipsum dolor sit amet consectetur. Hendrerit eget tempus arcu ultricies tellus dolor cras lorem non. Lectus aenean vitae integer vitae maecenas fames enim malesuada bibendum. Fringilla nulla tortor nunc sed aliquam turpis nisl felis. Hendrerit congue egestas lobortis leo eget vulputate. Diam a eu nec tempus. Magna lobortis est ante at maecenas quam in amet nisl. Consequat nec vulputate enim urna iaculis quisque malesuada nulla eu. Augue aliquet accumsan ac nunc venenatis pretium tortor pharetra.</p>
-						</div>
-					</Col>
-					<Col className="flex items-stretch" cols={{ lg: 4, md: 3, sm: 4 }}>
-						<Card
-							imageUrl={'/img/image.jpg'}
-							title={'Product name'}
-							description={'Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.'}
-						/>
-					</Col>
-					<Col className="flex items-stretch" cols={{ lg: 4, md: 3, sm: 4 }}>
-						<Card
-							imageUrl={'/img/image.jpg'}
-							title={'Product name'}
-							description={'Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.'}
-						/>
-					</Col>
-					<Col className="flex items-stretch" cols={{ lg: 4, md: 3, sm: 4 }}>
-						<Card
-							imageUrl={'/img/image.jpg'}
-							title={'Product name'}
-							description={'Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.'}
-						/>
-					</Col>
-					<Col className="flex items-stretch" cols={{ lg: 4, md: 3, sm: 4 }}>
-						<Card
-							imageUrl={'/img/image.jpg'}
-							title={'Product name'}
-							description={'Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.'}
-						/>
-					</Col>
-					<Col className="flex items-stretch" cols={{ lg: 4, md: 3, sm: 4 }}>
-						<Card
-							imageUrl={'/img/image.jpg'}
-							title={'Product name'}
-							description={'Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.'}
-						/>
-					</Col>
-					<Col className="flex items-stretch" cols={{ lg: 4, md: 3, sm: 4 }}>
-						<Card
-							imageUrl={'/img/image.jpg'}
-							title={'Product name'}
-							description={'Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.'}
-						/>
-					</Col>
-					<Col className="flex items-stretch" cols={{ lg: 4, md: 3, sm: 4 }}>
-						<Card
-							imageUrl={'/img/image.jpg'}
-							title={'Product name'}
-							description={'Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.'}
-						/>
-					</Col>
-					<Col className="flex items-stretch" cols={{ lg: 4, md: 3, sm: 4 }}>
-						<Card
-							imageUrl={'/img/image.jpg'}
-							title={'Product name'}
-							description={'Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.'}
-						/>
-					</Col>
-					<Col className="flex items-stretch" cols={{ lg: 4, md: 3, sm: 4 }}>
-						<Card
-							imageUrl={'/img/image.jpg'}
-							title={'Product name'}
-							description={'Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.'}
-						/>
-					</Col>
-				</Container>
+				<ProductsList />
 			</section>
 			<section id="parrilla" className="parrilla">
 				<Container>
