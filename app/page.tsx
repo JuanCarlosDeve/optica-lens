ebb602e7699570c8801962c2f19907e75475b37c
JohanBap <johangregoxbox577gol@gmail.com>
2023-09-29 18:28:48 -0400

    se realizo la section logo delivery y parrilla

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Container, Col } from "@citrica/objects";
 import Card from "@/components/citrica-ui/card";
 import Navbar from "@/components/citrica-ui/navbar";
+import Link from "next/link";
 
 export default function Home() {
 	return (
@@ -101,26 +102,52 @@ export default function Home() {
 				</Container>
 			</section>
 			<section id="parrilla" className="parrilla">
-				<Container>
-					<Col cols={{ lg: 12, md: 6, sm: 4 }} >
-						<div className="h-44 flex flex-col-reverse justify-center items-center">
+				<Container >
+					<Col cols={{ lg: 12, md: 6, sm: 4 }} className="center mb-14">
+						<div className=" flex mt-16 mb-14 flex-col-reverse justify-center items-center">
 							<picture>
 								<img src={'/img/mi-parrilla.svg'} alt="" />
 							</picture>
 						</div>
-						<p className="copy py-5">Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed. Massa scelerisque sed dignissim in amet mi donec morbi nulla. Elementum lorem fermentum at nulla odio porttitor. Posuere suspendisse cras malesuada aenean. </p>
+						<Container>
+							<Col cols={{ lg: 12, md: 6, sm: 4 }} className="mb-[32px]" >
+								<h2 className="flex items-center mb-11 justify-start headline text-gold-brand">
+									Lorem ipsum
+								</h2>
+								<p className="flex text-left copy py-5">
+									Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed. Massa scelerisque sed dignissim in amet mi donec morbi nulla. Elementum lorem fermentum at nulla odio porttitor. Posuere suspendisse cras malesuada aenean.
+								</p>
+							</Col>
+							<Col cols={{ lg: 12, md: 6, sm: 4 }} className="flex justify-end" >
+							<Link className=" flex justify-center bg-black-brand text-gold-lightest button items-center  border-2 rounded-xl w-[179px] h-[40px]" href='Ver-todos'>
+						Visitar
+					</Link>
+							</Col>
+						</Container>
 					</Col>
 				</Container>
 			</section>
-			<section id="delivery" className="delivery">
-				<Container>
-					<Col cols={{ lg: 12, md: 6, sm: 4 }} >
-						<div className="h-44 flex flex-col-reverse justify-center items-center">
-							<picture>
-								<img src={'###'} alt="" />
-							</picture>
+			<section id="delivery" className="our-products">
+				<Container >
+					<Col cols={{ lg: 12, md: 6, sm: 4 }} className="center mb-14 ">
+						<div className=" flex mt-16 mb-14 headline justify-center items-center">
+							<h2>Logo Jordi Delivery</h2>
 						</div>
-						<p className="copy py-5">Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed. Massa scelerisque sed dignissim in amet mi donec morbi nulla. Elementum lorem fermentum at nulla odio porttitor. Posuere suspendisse cras malesuada aenean. </p>
+						<Container>
+							<Col cols={{ lg: 12, md: 6, sm: 4 }} className="mb-[32px]" >
+								<h2 className="flex items-center mb-11 justify-start headline text-gold-brand">
+									Lorem ipsum
+								</h2>
+								<p className="flex text-left copy py-5">
+									Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed. Massa scelerisque sed dignissim in amet mi donec morbi nulla. Elementum lorem fermentum at nulla odio porttitor. Posuere suspendisse cras malesuada aenean.
+								</p>
+							</Col>
+							<Col cols={{ lg: 12, md: 6, sm: 4 }} className="flex  justify-end" >
+								<Link className=" flex justify-center bg-black-brand text-gold-lightest button items-center  border-2 rounded-xl w-[179px] h-[40px]" href='Ver-todos'>
+									Visitar
+								</Link>
+							</Col>
+						</Container>
 					</Col>
 				</Container>
 			</section>
@@ -151,12 +178,22 @@ export default function Home() {
 							<img width={194} height={165} src={'/img/Logoipsum4.svg'} alt="" />
 						</picture>
 					</Col>
-					<Col cols={{ lg: 2, md: 3, sm: 4 }} className="center mb-14">
+					<Col cols={{ lg: 2, md: 3, sm: 4 }} className="center mb-14 only-lg">
+						<picture>
+							<img width={194} height={165} src={'/img/Logoipsum5.svg'} alt="" />
+						</picture>
+					</Col>
+					<Col cols={{ lg: 12, md: 5, sm: 4 }} className=" flex justify-start mb-14 not-lg">
 						<picture>
 							<img width={194} height={165} src={'/img/Logoipsum5.svg'} alt="" />
 						</picture>
 					</Col>
 				</Container>
+
+				
+				
+				
+				
 				<Container>
 					<Col cols={{ lg: 6, md: 6, sm: 4 }} >
 						<p className="body py-5">Lorem ipsum dolor sit amet consectetur. Semper mauris libero enim sed aenean a nunc sed in. Ac mattis venenatis id viverra feugiat. Nunc pellentesque malesuada ipsum lectus felis. Nulla leo auctor diam tristique adipiscing. Pharetra sed nibh viverra sed. Est nunc amet luctus tellus ut nulla eget iaculis enim. Sed aenean tincidunt rhoncus et turpis et ac. Id sed ut adipiscing odio risus nulla. Malesuada. </p>
