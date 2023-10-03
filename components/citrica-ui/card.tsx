ff064f6db4a0a7b8555b1e07ff67471f706c3037
Bernardo Borrat <bernardo.borrat@bborrat.com>
2023-10-02 23:38:14 -0300

    Product cards styles

--- components/citrica-ui/card.tsx
+++ components/citrica-ui/card.tsx
@@ -26,17 +26,17 @@ const Card = ({ imageUrl, title, description, btnLabel, btnAction, url2 }: CardP
 
 
   return (
-    <Link href={""} onClick={openModal} scroll={false} className="flex flex-col bg-[rgba(255,255,255,1)] p-10 mb-8 card-container">
+    <Link href={""} onClick={openModal} scroll={false} className="flex flex-col bg-[rgba(255,255,255,1)] p-6 mb-8 card-container">
       {imageUrl && (
         <picture  className="flex justify-center">
           <img  className="rounded-lg" src={imageUrl} alt="" />
         </picture>
       )}
       {title && (
-        <h2 className=" mt-10 title-medium">{title}</h2>
+        <h2 className=" mt-5 title text-gold-brand">{title}</h2>
       )}
       {description && (
-        <p className="copy-medium mt-10">{description}</p>
+        <p className="copy-medium mt-3">{description}</p>
       )}
       {/* {btnAction && btnLabel && (
         <div className="mt-8 flex justify-stard items-end flex-auto">
