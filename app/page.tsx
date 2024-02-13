23404709fac56783f0fea9179335a1bdc023cbfb
Juan Carlos <juancarlosvigar@gmail.com>
2024-02-13 08:21:10 -0500

    se agrega contenido

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
 				<Container>
 					<Col cols={{ lg: 8, md: 6, sm: 4 }} className="">
 						<div className="flex flex-col h-screen justify-center gap-4 pb-10">
-							<picture className=" flex relative bottom-20 "> 
-							{/* ojo    */}
+							<picture className=" flex relative bottom-20 ">
+								{/* ojo    */}
 								<img className="logo-prime-grill " src={'/img/ojohorus.png'} alt='logo' />
 							</picture>
 							<h2 className="display text-gold-brand">
@@ -32,16 +32,29 @@ export default function Home() {
 						</div>
 						<Container>
 							<Col cols={{ lg: 12, md: 6, sm: 4 }} className="mb-[32px]" >
-								
-								<p className="flex text-left copy py-5">
-								Integrantes: <br/> Jackelin Calderón  25.990.283<br/>Yesbeli Marcano 31.926.903<br/>Yolennys Nuñez 24.015.166<br/>Norvelys Arias 25.516.927<br/>Maria Vivas 16.564.313
-									
-								</p>
 
 								<p className="flex text-left copy py-5">
-									Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam excepturi officiis veniam itaque deserunt explicabo libero fugiat? Quisquam sapiente deserunt error temporibus, magnam accusantium nulla hic amet dolorum iste at?
+									Integrantes: <br /> Jackelin Calderón  25.990.283<br />Yesbeli Marcano 31.926.903<br />Yolennys Nuñez 24.015.166<br />Norvelys Arias 25.516.927<br />Maria Vivas 16.564.313
 
 								</p>
+
+								<h2 className="flex items-center justify-start headline text-gold-brand">Plan de Negocios</h2>
+
+								<p className="flex text-left copy py-5"> El primer aspecto es identificar la Óptica, en el caso que analizamos esOpti Lent&apos;s, el imagotipo es el nombre de la Óptica y el ojo de Horus, uno de los símbolos del antiguo Egipto, se cree que es un símbolo que proporciona protección, salud y rejuvenecimiento.
+								</p>
+								<p className="flex text-left copy py-5">La ubicación y la distribución de los espacios de acuerdo al uso, en este caso es en el centro
+									comercial Sambil, Chacao, piso 1, local #12</p>
+								<p className="flex text-left copy py-5">Plan de marketing: Incluye la publicidad, para lo cual se implementara las TIC, enfocándolo a
+									Redes Sociales y pagina web corporativa donde puede emplearse el Hosting Virtual Private
+									Server (VPS) y también el Vercel.</p>
+								<p className="flex text-left copy py-5">Vercel es un servicio de alojamiento web que se especializa en despliegue rápido y sencillo de
+									aplicaciones web y sitios estáticos. Aunque no es exactamente un VPS (Servidor Privado
+									Virtual), comparte algunas similitudes y diferencias:</p>
+								<p className="flex text-left copy py-5">En cuanto a las similitudes se encuentran: el aislamiento: Al igual que un VPS, Vercel
+									proporciona un entorno aislado para tus aplicaciones. Cada proyecto se ejecuta en su propio
+									espacio, lo que garantiza que los recursos no se compartan con otros usuarios. El control: Tienes
+									control total sobre la configuración de la aplicación, incluidas las variables de entorno, las rutas y
+									las redirecciones.</p>
 							</Col>
 							{/* <Col cols={{ lg: 12, md: 6, sm: 4 }} className="flex  justify-end" >
 								<Link className=" flex justify-center bg-black-brand text-gold-lightest label items-center  border-2 rounded-md w-[179px] h-[40px]" href='Ver-todos'>
@@ -57,7 +70,7 @@ export default function Home() {
 					<Col cols={{ lg: 12, md: 6, sm: 4 }}>
 						<div className="py-6">
 							<h2 className="headline text-white">Misión</h2>
-							<p className="title text-white">En Opti Lents, nuestra misión es proporcionar a nuestros clientes una experiencia integral en cuidado visual, ofreciendo una amplia gama de productos de calidad, desde lentes de contacto hasta gafas de sol, respaldados por un servicio excepcional y tecnología de vanguardia.<br/> Nos comprometemos a satisfacer las necesidades individuales de cada cliente, brindando exámenes visuales precisos, asesoramiento experto y soluciones personalizadas para mejorar su salud ocular y su calidad de vida.<br/>  Nos esforzamos por mantenernos a la vanguardia de la industria óptica, garantizando la excelencia en cada aspecto de nuestro negocio y promoviendo la importancia de la salud visual en nuestra comunidad.</p>
+							<p className="title text-white">En Opti Lents, nuestra misión es proporcionar a nuestros clientes una experiencia integral en cuidado visual, ofreciendo una amplia gama de productos de calidad, desde lentes de contacto hasta gafas de sol, respaldados por un servicio excepcional y tecnología de vanguardia.<br /> Nos comprometemos a satisfacer las necesidades individuales de cada cliente, brindando exámenes visuales precisos, asesoramiento experto y soluciones personalizadas para mejorar su salud ocular y su calidad de vida.<br />  Nos esforzamos por mantenernos a la vanguardia de la industria óptica, garantizando la excelencia en cada aspecto de nuestro negocio y promoviendo la importancia de la salud visual en nuestra comunidad.</p>
 						</div>
 					</Col>
 				</Container>
@@ -67,13 +80,13 @@ export default function Home() {
 					<Col cols={{ lg: 6, md: 6, sm: 4 }} className="mision-vision-item-left only-lg" >
 						<div className="py-6">
 							<h2 className="headline text-white">Misión</h2>
-							<p className="title text-white">En Opti Lents, nuestra misión es proporcionar a nuestros clientes una experiencia integral en cuidado visual, ofreciendo una amplia gama de productos de calidad, desde lentes de contacto hasta gafas de sol, respaldados por un servicio excepcional y tecnología de vanguardia.<br/> Nos comprometemos a satisfacer las necesidades individuales de cada cliente, brindando exámenes visuales precisos, asesoramiento experto y soluciones personalizadas para mejorar su salud ocular y su calidad de vida.<br/>  Nos esforzamos por mantenernos a la vanguardia de la industria óptica, garantizando la excelencia en cada aspecto de nuestro negocio y promoviendo la importancia de la salud visual en nuestra comunidad.</p>
+							<p className="title text-white">En Opti Lents, nuestra misión es proporcionar a nuestros clientes una experiencia integral en cuidado visual, ofreciendo una amplia gama de productos de calidad, desde lentes de contacto hasta gafas de sol, respaldados por un servicio excepcional y tecnología de vanguardia.<br /> Nos comprometemos a satisfacer las necesidades individuales de cada cliente, brindando exámenes visuales precisos, asesoramiento experto y soluciones personalizadas para mejorar su salud ocular y su calidad de vida.<br />  Nos esforzamos por mantenernos a la vanguardia de la industria óptica, garantizando la excelencia en cada aspecto de nuestro negocio y promoviendo la importancia de la salud visual en nuestra comunidad.</p>
 						</div>
 					</Col>
 					<Col cols={{ lg: 6, md: 6, sm: 4 }}>
 						<div className="py-6">
 							<h2 className="headline text-gold-brand">Visión</h2>
-							<p className="title">En Opti Lents, aspiramos a ser líderes reconocidos en el sector óptico, distinguidos por nuestra excelencia en productos, servicios y atención al cliente.<br/>  Nos esforzamos por expandir nuestra presencia en el mercado nacional e internacional, estableciendo alianzas estratégicas con proveedores de calidad y utilizando tecnologías innovadoras para ofrecer soluciones visuales de vanguardia.<br/>  Buscamos ser un referente en la promoción de la salud visual, educando a la comunidad sobre la importancia del cuidado de los ojos y contribuyendo al bienestar general de nuestros clientes.<br/>  Nuestra visión es ser la primera opción para quienes buscan soluciones ópticas confiables, personalizadas y de alta calidad.</p>
+							<p className="title">En Opti Lents, aspiramos a ser líderes reconocidos en el sector óptico, distinguidos por nuestra excelencia en productos, servicios y atención al cliente.<br />  Nos esforzamos por expandir nuestra presencia en el mercado nacional e internacional, estableciendo alianzas estratégicas con proveedores de calidad y utilizando tecnologías innovadoras para ofrecer soluciones visuales de vanguardia.<br />  Buscamos ser un referente en la promoción de la salud visual, educando a la comunidad sobre la importancia del cuidado de los ojos y contribuyendo al bienestar general de nuestros clientes.<br />  Nuestra visión es ser la primera opción para quienes buscan soluciones ópticas confiables, personalizadas y de alta calidad.</p>
 						</div>
 					</Col>
 				</Container>
@@ -162,7 +175,7 @@ export default function Home() {
 				<Container className="flex justify-center">
 					<Col cols={{ lg: 12, md: 6, sm: 4 }} noPadding>
 						<div className=" flex flex-col h-full">
-						<Carousel/>
+							<Carousel />
 						</div>
 					</Col>
 				</Container>
@@ -170,24 +183,24 @@ export default function Home() {
 			<section id="sedes" className="boutiques ">
 				<div className="backdrop-blur-md">
 
-				<Container >
-					<Col cols={{ lg: 12, md: 6, sm: 4 }} className="center ">
-						<h2 className="headline  mt-28 text-gold-brand">SEDE</h2>
-					</Col>
-				</Container>
-				<Container className="flex justify-between ">
-					<Col cols={{ lg: 12, md: 6, sm: 4 }} className="center">
-						<div className="pt-10">
-							<a href="https://maps.app.goo.gl/cRJHczsoUHdU4rCo9" target="_blank"> 
-							<picture className="hover:cursor-pointer">
-								<img src="/img/icons/location_on.svg" alt="" />
-							</picture>
-							</a>
-							<h2 className="title text-gold-brand">Caracas</h2>
-							<p className="body text-black mt-10">Centro comercial Sambil Chacao, piso 1 local #12</p>
-						</div>
-					</Col>
-					{/* <Col cols={{ lg: 4, md: 6, sm: 4 }} className="center">
+					<Container >
+						<Col cols={{ lg: 12, md: 6, sm: 4 }} className="center ">
+							<h2 className="headline  mt-28 text-gold-brand">SEDE</h2>
+						</Col>
+					</Container>
+					<Container className="flex justify-between ">
+						<Col cols={{ lg: 12, md: 6, sm: 4 }} className="center">
+							<div className="pt-10">
+								<a href="https://maps.app.goo.gl/cRJHczsoUHdU4rCo9" target="_blank">
+									<picture className="hover:cursor-pointer">
+										<img src="/img/icons/location_on.svg" alt="" />
+									</picture>
+								</a>
+								<h2 className="title text-gold-brand">Caracas</h2>
+								<p className="body text-black mt-10">Centro comercial Sambil Chacao, piso 1 local #12</p>
+							</div>
+						</Col>
+						{/* <Col cols={{ lg: 4, md: 6, sm: 4 }} className="center">
 						<div className="pt-10">
 							<picture>
 								<img src="/img/icons/location_on.svg" alt="" />
@@ -205,7 +218,7 @@ export default function Home() {
 							<p className="body text-white mt-10">Jr. Sargento Lores 889</p>
 						</div>
 					</Col> */}
-				</Container>
+					</Container>
 				</div>
 			</section>
 		</>
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -11,56 +11,56 @@ const allProducts = [
   {
     title: "Lentes de Sol",
     url: "/img/lentes-de-sol-polarizados.jpg",
-    description: "",
+    description: "Protege tus ojos del sol con estilo con nuestros lentes de sol. Diseñados para brindar una visión clara y cómoda mientras te destacas con elegancia.",
     url2: "",
     category_id: 1,
   },
   {
     title: "Lentes de Lectura",
     url: "/img/lentes-lectura.jpg",
-    description: "",
+    description: "Disfruta de la lectura con claridad y comodidad con nuestros lentes especializados. Diseñados para adaptarse a tu estilo de vida y necesidades visuales.",
     url2: "",
     category_id: 1,
   },
   {
     title: "Lentes con UV",
     url: "/img/lente-uv.jpeg",
-    description: "",
+    description: "Protege tus ojos de los dañinos rayos UV con nuestros lentes especiales. Mantén tu visión saludable y protegida mientras disfrutas del aire libre.",
     url2: "",
     category_id: 1,
   },
   {
     title: "Lentes para niños",
     url: "/img/lentes-nino.jpg",
-    description: "",
+    description: "Cuida los ojos de tus pequeños con nuestros lentes diseñados especialmente para ellos. Ofreciendo protección y estilo para los más jóvenes.",
     url2: "",
     category_id: 1,
   },
   {
     title: "Lentes para Deporte",
     url: "/img/lentes-deporte.jpg",
-    description: "",
+    description: "Optimiza tu rendimiento deportivo con nuestros lentes especializados. Diseñados para brindar comodidad y protección mientras te concentras en tus actividades favoritas.",
     url2: "",
     category_id: 1,
   },
   {
     title: "Lentes para PC",
     url: "/img/lentes-pc.jpg",
-    description: "",
+    description: "Protege tus ojos del cansancio digital con nuestros lentes para PC. Reduciendo la fatiga ocular y mejorando tu confort durante largas sesiones frente a la pantalla.",
     url2: "",
     category_id: 1,
   },
   {
     title: "Azul",
     url: "/img/contacto-azul.jpeg",
-    description: "",
+    description: "Experimenta la libertad visual con nuestros lentes de contacto azules. Brindando comodidad y estilo mientras disfrutas de una visión clara y sin obstáculos.",
     url2: "",
     category_id: 2,
   },
   {
     title: "Examen de la Vista",
     url: "/img/examenes.jpg",
-    description: "",
+    description: "Obtén una evaluación visual precisa con nuestro examen de la vista. Nuestros profesionales te ayudarán a mantener una visión saludable y nítida.",
     url2: "",
     category_id: 3,
   },
@@ -114,12 +114,12 @@ const ProductsList = () => {
       </Col>
       <Col cols={{ lg: 6, md: 6, sm: 4 }} className="">
         <div className="py-6">
-          <p className="text-white">Lorem ipsum dolor sit amet consectetur. Hendrerit eget tempus arcu ultricies tellus dolor cras lorem non. Lectus aenean vitae integer vitae maecenas fames enim malesuada bibendum. Fringilla nulla tortor nunc sed aliquam turpis nisl felis. Hendrerit congue egestas lobortis leo eget vulputate. Diam a eu nec tempus. Magna lobortis est ante at maecenas quam in amet nisl. Consequat nec vulputate enim urna iaculis quisque malesuada nulla eu. Augue aliquet accumsan ac nunc venenatis pretium tortor pharetra.</p>
+          <p className="text-white">Descubre nuestra amplia selección de productos ópticos diseñados para proteger y mejorar tu visión. Desde elegantes lentes de sol hasta prácticos lentes para PC, tenemos todo lo que necesitas para cuidar tus ojos con estilo. Explora nuestras opciones y encuentra el complemento perfecto para tu estilo de vida.</p>
         </div>
       </Col>
       <Col cols={{ lg: 6, md: 6, sm: 4 }} className="mb-14">
         <div className="py-6 ">
-          <p className="text-white">Lorem ipsum dolor sit amet consectetur. Hendrerit eget tempus arcu ultricies tellus dolor cras lorem non. Lectus aenean vitae integer vitae maecenas fames enim malesuada bibendum. Fringilla nulla tortor nunc sed aliquam turpis nisl felis. Hendrerit congue egestas lobortis leo eget vulputate. Diam a eu nec tempus. Magna lobortis est ante at maecenas quam in amet nisl. Consequat nec vulputate enim urna iaculis quisque malesuada nulla eu. Augue aliquet accumsan ac nunc venenatis pretium tortor pharetra.</p>
+          <p className="text-white">En Opti Lents, nos comprometemos a ofrecerte soluciones visuales de calidad que se adapten a tus necesidades únicas. Nuestros productos están diseñados con los más altos estándares de confort, estilo y funcionalidad, para que puedas disfrutar de una visión clara y sin esfuerzo en cualquier momento y lugar. ¡Descubre lo que tenemos para ofrecerte y cuida tus ojos con Opti Lents!</p>
         </div>
       </Col>
       <Col cols={{ lg: 12, md: 6, sm: 4 }} className="category-selector">
@@ -141,7 +141,7 @@ const ProductsList = () => {
           <Card
             imageUrl={product.url}
             title={product.title}
-            description={`Lorem ipsum dolor sit amet consectetur. Orci donec non rhoncus nulla volutpat. Vulputate dolor vestibulum arcu tincidunt sed.`}
+            description={product.description}
             url2={product.url2}
           />
         </Col>
